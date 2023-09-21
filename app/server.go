09f7aa4014858661f8e11615d3a3e858ba55c611
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-21 16:27:40 +0530

    pass stage 3
    parse HTTP get line

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"strings"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
@@ -20,18 +22,48 @@ func main() {
 		os.Exit(1)
 	}
 
-	connection, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		request := make([]byte, 1024)
+		_, err = connection.Read(request)
+		requestData := string(request)
+		fmt.Printf("Data: \n%s", requestData)
+		splitData := strings.Split(requestData, "\r\n")
+		for i, datum := range splitData {
+			fmt.Printf("i=%v -- Data: %v----\n", i, datum)
+		}
+		startLine := parseStartLine(splitData[0])
+		if startLine.Path == "/" {
+			connection.Write([]byte(("HTTP/1.1 200 OK\r\n\r\n")))
+		} else {
+			connection.Write([]byte(("HTTP/1.1 404 NOT FOUND\r\n\r\n")))
+		}
+
+		connection.Close()
 	}
+}
 
-	var read []byte
-	connection.Read(read)
+func parseStartLine(line string) StartLine {
+	items := strings.Split(line, " ")
+	if len(items) != 3 {
+		log.Fatal("Expect 'HTTP_METHOD<space>PATH<space>HTTP_VERSION'")
+	}
+	return StartLine{
+		HttpMethod:  items[0],
+		Path:        items[1],
+		HttpVersion: items[2],
+	}
+}
 
-	connection.Write([]byte(("HTTP/1.1 200 OK\r\n\r\n")))
-
-	connection.Close()
+type StartLine struct {
+	HttpMethod  string
+	Path        string
+	HttpVersion string
 }
 
 func handleErr(err error) {
