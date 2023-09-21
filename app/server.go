c4bec4b0f050abff587ab1a12d340604a646e592
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-21 16:42:39 +0530

    pass stage 4
    parse and respond to echo

--- app/server.go
+++ app/server.go
@@ -29,6 +29,7 @@ func main() {
 			os.Exit(1)
 		}
 
+		// process request
 		request := make([]byte, 1024)
 		_, err = connection.Read(request)
 		requestData := string(request)
@@ -37,15 +38,33 @@ func main() {
 		for i, datum := range splitData {
 			fmt.Printf("i=%v -- Data: %v----\n", i, datum)
 		}
+
 		startLine := parseStartLine(splitData[0])
+
 		if startLine.Path == "/" {
 			connection.Write([]byte(("HTTP/1.1 200 OK\r\n\r\n")))
+		} else if strings.HasPrefix(startLine.Path, "/echo") {
+			res := echoResponse(startLine.Path)
+			connection.Write([]byte((res)))
 		} else {
 			connection.Write([]byte(("HTTP/1.1 404 NOT FOUND\r\n\r\n")))
 		}
 
 		connection.Close()
 	}
+}
+
+func echoResponse(path string) string {
+	content := strings.TrimPrefix(path, "/echo/")
+	response := make([]string, 5)
+
+	response[0] = "HTTP/1.1 200 OK"
+	response[1] = "Content-Type: text/plain"
+	response[2] = fmt.Sprintf("Content-Length: %d", len(content))
+	response[3] = CONTENT_SEPARATOR
+	response[4] = content
+
+	return strings.Join(response, LINE_SEPARATOR)
 }
 
 func parseStartLine(line string) StartLine {
@@ -65,6 +84,12 @@ type StartLine struct {
 	Path        string
 	HttpVersion string
 }
+
+//goland:noinspection GoSnakeCaseUsage
+const LINE_SEPARATOR = "\r\n"
+
+//goland:noinspection GoSnakeCaseUsage
+const CONTENT_SEPARATOR = ""
 
 func handleErr(err error) {
 	if err != nil {
